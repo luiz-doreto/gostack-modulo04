86df134fd5639535d319a553a9a7b65e9bd3c2ed
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-10-24 21:31:02 -0300

    Módulo 4 - Finalizando modulo

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
     state = {
-        techs: [
-            'Node.js',
-            'ReactJs',
-            'React Native',
-        ],
+        newTech: '',
+        techs: [],
     };
 
+    componentDidMount() {
+        const techs = localStorage.getItem('techs');
+
+        if (techs) {
+            this.setState({ techs: JSON.parse(techs) });
+        } 
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { techs } = this.state;
+        if (prevState.techs !== techs) {
+            localStorage.setItem('techs', JSON.stringify(techs));
+        }
+    }
+
+    handleInputChange = e => {
+        this.setState({newTech: e.target.value});
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { techs, newTech } = this.state;
+        
+        this.setState({
+            techs: [...techs, newTech],
+            newTech: '',
+        });
+    }
+
+    handleDelete = tech => {
+        const { techs } = this.state;
+        this.setState({ techs: techs.filter(t => t !== tech)});
+    }
+
     render() {
-        console.log(this.state.techs);
         return (
-            <ul>
-                <li>Node.js</li>
-                <li>ReactJS</li>
-                <li>React Native</li>
-            </ul>
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                    {this.state.techs.map(tech => (
+                        <TechItem 
+                            key={tech} 
+                            tech={tech}
+                            onDelete={() => this.handleDelete(tech)}/>
+                    ))}
+                </ul>
+                <input 
+                    type="text"
+                    onChange={this.handleInputChange}
+                    value={this.state.newTech}/>
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
